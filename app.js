4dfb18ff13a914bf5c2e56de8179866b9682f48a
barrycumbie@gmail <barrycumbie@gmail.com>
2023-11-28 14:45:36 -0600

    added ejs view

--- app.js
+++ app.js
@@ -3,9 +3,18 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs');
+
+
 app.get('/', function (req, res) {
   res.send('Hello World from Node & Express')
 })
+
+app.get('/ejs', function (req, res) {
+    res.render("index", {
+        data: "my sever side data"
+    })
+  })
 
 app.get('/bazooka', function (req, res) {
     res.sendFile("index.html")
